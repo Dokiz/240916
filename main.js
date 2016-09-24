9fd47629cd65e3c051292e0b4ffe6577b8aed42a
Dokiz_228@mail.ru <deoptgqqr12>
2016-09-24 14:42:57 +0300

    start

---
+++ main.js
@@ -0,0 +1,21 @@
+var triangle={
+	ab:3,
+	bc:4,
+	ac:function(){
+		sss = (this.ab*this.ab)+(this.bc*this.bc);
+		return Math.sqrt(sss);
+	}
+};
+console.log(triangle.ac());
+
+var gipotenuza=(function(a,b){
+	aaa=(a*a)+(b*b);
+	return Math.sqrt(aaa);
+}(3,4));
+console.log(gipotenuza);
+
+var storona=function cc(aa,bb) {
+	gip=(aa*aa)+(bb*bb);
+	return Math.sqrt(gip);
+}
+console.log(storona(3,4));
